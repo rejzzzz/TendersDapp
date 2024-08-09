e36458dfa4ddce164f795a275e4521090af3ce42
Saitej2456 <138279877+Saitej2456@users.noreply.github.com>
2024-08-09 19:32:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginRegister from './Components/LoginRegister/LoginRegister';
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
